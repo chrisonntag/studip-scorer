11039892ae64b62b94c2b56c369ca19655a31648
Christoph Sonntag <info@chrisonntag.com>
2018-12-18 17:19:27 +0100

    Update start script

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-python3 studip_scorer/__init__.py
+WD="$(cd "$(dirname "$0")"; pwd -P)"
+SOURCE="$WD/env/bin/activate"
+MAIN="$WD/studip_scorer/__init__.py"
+
+source "${SOURCE}"
+python3 "${MAIN}"
